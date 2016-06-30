702ed34a98070f999c99dd8bd2a4ea09d5840365
Stephen Romano <sarrooney10@gmail.com>
2016-06-30 01:31:15 -0400

    Added additional test cases
    Tested to make sure basic functionality of pushing a voters choice back in the list of choices

--- main.cpp
+++ main.cpp
@@ -1,14 +1,26 @@
 #include <iostream>
 #include "Vote.h"
 #include <string>
+#include <vector>
 
 using namespace std;
 
 int main(){
 
 	Vote Bob("Bob", 123);
-	cout << Bob.getName() << Bob.getID() << Bob.getParty();
-	Bob.setAffiliation("Democrat");
-	cout << Bob.getParty();
+	//cout << Bob.getName() << Bob.getID() << Bob.getParty();
+	//Bob.setAffiliation("Democrat");
+	//cout << Bob.getParty();
+	vector<string> bobsvotes;
+	bobsvotes.push_back("Bernie");
+	bobsvotes.push_back("Hilary");
+	bobsvotes.push_back("Trump");
+	bobsvotes.push_back("Johnson");
+	bobsvotes.push_back("Cruz");
+	Bob.castVote(bobsvotes);
+	cout << Bob.getVote() << endl;
+	Bob.pushVoteNum();
+	cout << Bob.getVote() << endl;
+
 	return 0;
 }
